8dd414a26ca6b4511967d66f2f32755bcc66fd45
pyygithub <panyangyang@thtf.com.cn>
2019-09-26 17:30:56 +0800

    项目初始化

---
+++ thtf-ui/src/router.js
@@ -0,0 +1,26 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+Vue.use(Router)
+
+export default new Router({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: () => import(/* webpackChunkName: "about" */ './views/Home.vue')
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: () => import(/* webpackChunkName: "about" */ './views/Login.vue')
+    },
+    {
+        path: '/404',
+        name: 'notFound',
+        component: () => import(/* webpackChunkName: "about" */ './views/Error/404.vue')
+    }
+  ]
+})
